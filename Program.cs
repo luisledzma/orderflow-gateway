e5d98aed168e71db9867e7446cde3d53ea7591b1
luisledzma <luisledzma@outlook.com>
2025-02-13 21:29:22 -0600

    Add project configuration and Health Controller

---
+++ Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Yarp.ReverseProxy.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+//Add controllers
+builder.Services.AddControllers();
+
+// Add YARP Reverse Proxy
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.WebHost.UseUrls("http://0.0.0.0:5050");
+var app = builder.Build();
+
+// Enable routing
+app.UseRouting();
+
+// Enable Reverse Proxy
+app.MapReverseProxy();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
