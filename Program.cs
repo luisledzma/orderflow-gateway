9fff939efc6f6a30d6a0ac5d6755849dca44b613
luisledzma <luisledzma@outlook.com>
2025-03-05 15:16:31 -0600

    Add serilog

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using Yarp.ReverseProxy.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
